f4987cf1d9e482804458371a869716af6f770e4f
Richard Ayikwei <richardayikweibee@gmail.com>
2025-08-13 10:04:16 +0000

    init uv and added whatsapp dataframe created

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     author="Ayikwei Richard",
     description="Custom sentiment package for analysing swap related messages",
     name="sentiment",
-    version="0.1.2",
+    version="0.2.2",
     packages=find_packages(include=["sentiment", "sentiment.*"]),
     install_requires=['pandas','re'],
 )
