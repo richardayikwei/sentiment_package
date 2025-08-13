3232b1ec4bdebc987d69ff0cac4d31982e2f26c3
Richard Ayikwei <richardayikweibee@gmail.com>
2025-08-13 12:26:31 +0000

    adjusting root dir, package version

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     author="Ayikwei Richard",
     description="Custom sentiment package for analysing swap related messages",
     name="sentiment",
-    version="0.2.2",
-    packages=find_packages(include=["sentiment", "sentiment.*"]),
+    version="0.2.3",
+    packages=find_packages(where='src',include=["sentiment", "sentiment.*"]),
+    package_dir={"": "src"},
     install_requires=['pandas','re'],
 )
