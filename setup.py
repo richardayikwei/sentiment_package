ce654bb65b0627e0990149035f0871d1c7afb330
Richard Ayikwei <richardayikweibee@gmail.com>
2025-08-14 08:56:56 +0000

    chore: update version number in setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     author="Ayikwei Richard",
     description="Custom sentiment package for analysing swap related messages",
     name="evmotosentiment",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(where='src',include=["evmotosentiment", "evmotosentiment.*"]),
     package_dir={"": "src"},
     install_requires=['pandas','re'],
